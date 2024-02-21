219468d983c49a2c3a951668cb689f4d7d7bc592
TonysirShen <142557086+TonysirShen@users.noreply.github.com>
2024-02-21 02:04:59 +0000

    finished

--- main.py
+++ main.py
@@ -48,7 +48,32 @@ def quadratic_multiply(x, y):
     # this just converts the result from a BinaryNumber to a regular int
     return _quadratic_multiply(x,y).decimal_val
 
-def _quadratic_multiply(x, y):
+def _quadratic_multiply(x, y): #x,y are Binarynumber
+  
+    xvec = x.binary_vec
+    yvec = y.binary_vec
+    xvec,yvec = pad(xvec,yvec)
+    n = len(xvec)
+    if x.decimal_val <= 1 and y.decimal_val <=1:
+      return     BinaryNumber(x.decimal_val*y.decimal_val)
+    else:
+      l_xvec,r_xvec = split_number(xvec)
+      l_yvec,r_yvec = split_number(yvec)
+    #a = xl*yl
+    a = _quadratic_multiply(l_xvec,l_yvec)
+  #b = xr *y l 
+    b = _quadratic_multiply(BinaryNumber(l_xvec.decimal_val + r_xvec.decimal_val),BinaryNumber(l_yvec.decimal_val+r_yvec.decimal_val))
+   #d = xl*yr
+  #c = xr*yr
+    c = _quadratic_multiply(r_xvec,r_yvec)
+  # result = 2^(n/2) *a + 2^(n/2)(b-a-c) + c
+
+    result = bit_shift(a,n).decimal_val + bit_shift(BinaryNumber(b.decimal_val -a.decimal_val - c.decimal_val),n//2).decimal_val + c.decimal_val
+  
+      
+    
+    
+    return BinaryNumber(result)
     ### TODO
     pass
     ###
@@ -56,13 +81,14 @@ def _quadratic_multiply(x, y):
 
     
     
-def test_quadratic_multiply(x, y, f):
+def time_quadratic_multiply(x, y, f):
     start = time.time()
+    print(f(BinaryNumber(x),BinaryNumber(y)));
     # multiply two numbers x, y using function f
     
     return (time.time() - start)*1000
 
+print(time_quadratic_multiply(3,6,quadratic_multiply))
 
-    
     
 
